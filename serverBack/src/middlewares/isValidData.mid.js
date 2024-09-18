4fe572e5429849a0b632dcd1fdc8abf625bda5a5
cruiz386 <cruiz@teco.com.ar>
2024-09-18 20:36:06 -0300

    cond isValidData

--- serverBack/src/middlewares/isValidData.mid.js
+++ serverBack/src/middlewares/isValidData.mid.js
@@ -3,17 +3,21 @@ function isValidData(req, res, next) {
     // pero ademas necesita depender de next
     // que es la funcion encargada de "pasar" a la siguiente función (de middleware o controlador)
     try {
-      const { title, stock, price } = req.body;
-      if (!title || !stock || !price) {
-        const error = new Error("Title, stock and price are required");
-        error.statusCode = 400;
-        throw error;
-      } else {
+        const { title, stock, price } = req.body;
+        if (!title || typeof title !== 'string') {
+          throw new Error("Title is required and must be a string");
+        }
+        if (!price || isNaN(price) || price <= 0) {
+          throw new Error("Price is required and must be a positive number");
+        }
+        if (!stock || isNaN(stock) || stock < 0) {
+          throw new Error("Stock is required and must be a non-negative integer");
+        }
         return next();
+      } catch (error) {
+        error.statusCode = 400;
+        return next(error);
       }
-    } catch (error) {
-      throw error;
-    }
   }
   
   export default isValidData;
