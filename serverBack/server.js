5d24ee5c477f0a57521b136cf39616058d5c337c
cruiz386 <cruiz@teco.com.ar>
2024-09-25 20:17:40 -0300

    push branch dev

--- serverBack/server.js
+++ serverBack/server.js
@@ -2,15 +2,26 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { engine } from "express-handlebars";
+import { Server } from "socket.io";
+import { createServer } from "http";
 import router from "./src/routers/index.router.js";
 import errorHandler from "./src/middlewares/errorHandler.mid.js";
 import pathHandler from "./src/middlewares/pathHandler.mid.js";
 import __dirname from "./utils.js";
 
+
 try {
   const server = express();
   const PORT = 8080;
   const ready = () => console.log("server ready on port " + PORT);
+  const httpServer = createServer(server);
+  const tcpServer = new Server(httpServer);
+
+  tcpServer.on("connection", (socket) =>
+  console.log(`client ${socket.id} connected`)
+);
+
+
   server.listen(PORT, ready);
 
   server.use(morgan("dev"));
@@ -20,11 +31,14 @@ try {
   server.use(cors());
   server.use("/public", express.static("public"))
 
+
+
+
   server.engine("handlebars", engine());
   server.set("view engine", "handlebars");
   server.set("views", __dirname + "/src/views");
 
-  server.use( router)
+  server.use(router)
   server.use(errorHandler);
   server.use(pathHandler);
 
